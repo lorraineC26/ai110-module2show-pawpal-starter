4b8bd380b0fe582daef2a809d1d6f2d0c3fee952
lorraineC26 <lorraine.quinyee@gmail.com>
2026-03-29 20:59:46 -0400

    chore: add class skeletons from UML

---
+++ pawpal_system.py
@@ -0,0 +1,64 @@
+# "logic layer" where all your backend classes live
+
+class Owner:
+    def __init__(self, name, time_available, preferences):
+        self.name = name
+        self.time_available = time_available
+        self.preferences = preferences
+
+    def add_pet(self, pet):
+        pass
+
+    def get_summary(self):
+        pass
+
+
+class Pet:
+    def __init__(self, name, species, age, health_notes):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.health_notes = health_notes
+
+    def get_profile(self):
+        pass
+
+
+class Task:
+    def __init__(self, name, category, duration, priority, preferred_time):
+        self.name = name
+        self.category = category
+        self.duration = duration
+        self.priority = priority
+        self.preferred_time = preferred_time
+
+    def edit(self, field, value):
+        pass
+
+    def to_dict(self):
+        pass
+
+
+class Schedule:
+    def __init__(self, date, tasks, total_duration, reasoning):
+        self.date = date
+        self.tasks = tasks
+        self.total_duration = total_duration
+        self.reasoning = reasoning
+
+    def display(self):
+        pass
+
+    def get_reasoning(self):
+        pass
+
+
+class Scheduler:
+    def __init__(self, owner):
+        self.owner = owner
+
+    def generate_schedule(self):
+        pass
+
+    def explain_plan(self):
+        pass
